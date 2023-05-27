87a2df3cb730eb7252bf79a42e15702fbdc0490f
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-27 12:42:15 +0700

    Spinner & Popular TV Shows

--- js/script.js
+++ js/script.js
@@ -3,17 +3,27 @@ const global = {
 };
 // Fetch the data from API
 async function fetchAPIData(endpoint) {
+  // Register your key at: https://www.themoviedb.org/settings/api and enter here
+  // Only use this for development or very small projects. You should store your key and make requests from a server
+
   const API_KEY = 'fd8d3dab48909db84857be6066ec11f1';
   const API_URL = 'https://api.themoviedb.org/3/';
-
+  showSpinner();
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
+  hideSpinner();
   return data;
 }
-// Display popular movies
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
+// Display 20 popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('/movie/popular');
   results.forEach((movie) => {
@@ -47,6 +57,40 @@ async function displayPopularMovies() {
   });
   console.log(results);
 }
+// Display 20 popular TV Shows
+async function displayPopularTVShows() {
+  const { results } = await fetchAPIData('/tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="tv-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `<img
+          src='https://image.tmdb.org/t/p/w500${show.poster_path}'
+          class="card-img-top"
+          alt='${show.name}'
+        />`
+            : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt='${show.name}'
+        />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air Date: ${show.first_air_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.getElementById('popular-shows').appendChild(div);
+  });
+  console.log(results);
+}
 // Highlight active link:
 function highlightActiveLink() {
   const linkList = document.querySelectorAll('.nav-link');
@@ -64,7 +108,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('TV Shows');
+      displayPopularTVShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
