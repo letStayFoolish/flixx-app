833966c12e40d2376d45f139891e2626bc634d73
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-27 14:27:08 +0700

    TV Show Details Page

--- js/script.js
+++ js/script.js
@@ -154,7 +154,67 @@ async function displayMovieDetails() {
   `;
   document.getElementById('movie-details').appendChild(div);
 }
+// Display TV Show Details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
 
+  const show = await fetchAPIData(`tv/${showId}`);
+  // Overlay for background image
+  displayBackdrop('show', show.backdrop_path);
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+      ${
+        show.poster_path
+          ? `<img
+        src='https://image.tmdb.org/t/p/w500${show.poster_path}'
+        class="card-img-top"
+        alt='${show.name}'
+      />`
+          : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt='${show.name}'
+      />`
+      }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+        <p>${show.overview}</p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${
+          show.homepage
+        }" target="_blank" class="btn">Visit show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number of Episodes:</span> ${
+          show.number_of_episodes
+        }</li>
+        <li><span class="text-secondary">Last Episode To Air:</span> ${
+          show.last_episode_to_air.name
+        }</li>
+        <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+        ${show.production_companies
+          .map((company) => `<div lass="list-group">${company.name}</div>`)
+          .join('')}
+    </div>
+  `;
+  document.getElementById('show-details').appendChild(div);
+}
 // Function to show Backdrop on Details Page
 function displayBackdrop(type, backdropUrl) {
   const overlayDiv = document.createElement('div');
@@ -170,7 +230,7 @@ function displayBackdrop(type, backdropUrl) {
   overlayDiv.style.top = '0';
   overlayDiv.style.left = '0';
   overlayDiv.style.zIndex = '-1';
-  overlayDiv.style.opacity = '0.2';
+  overlayDiv.style.opacity = '0.25';
 
   if (type === 'movie') {
     document.getElementById('movie-details').appendChild(overlayDiv);
@@ -201,7 +261,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Show Details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
