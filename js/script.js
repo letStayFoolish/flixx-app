b77d42fc9547dcf44490d8a9e3abd235b490f6d6
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-27 21:03:41 +0700

    Display Search Results

--- js/script.js
+++ js/script.js
@@ -272,15 +272,58 @@ async function search() {
 
   if (global.search.term !== '' && global.search.term !== null) {
     // @todo - make request and display results
-    const results = await searchApiData();
-    console.log(results);
+    const { results, total_pages, page } = await searchApiData();
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+    displaySearchResults(results);
+    document.getElementById('search-term').value = '';
   } else {
     showAlert('Please enter a search term.');
   }
-  console.log(urlParams);
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="${global.search.type}-details.html?id=${result.id}">
+        ${
+          result.poster_path
+            ? `<img
+          src='https://image.tmdb.org/t/p/w500/${result.poster_path}'
+          class="card-img-top"
+          alt='${global.search.type === 'movie' ? result.title : result.name}'
+        />`
+            : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt='${global.search.type === 'movie' ? result.title : result.name}'
+        />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${
+          global.search.type === 'movie' ? result.title : result.name
+        }</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${
+            global.search.type === 'movie'
+              ? result.release_date
+              : result.first_air_date
+          }</small>
+        </p>
+      </div>
+    `;
+
+    document.getElementById('search-results').appendChild(div);
+  });
 }
 // Show Alert
-function showAlert(message, className) {
+function showAlert(message, className = 'alert-error') {
   const alertEl = document.createElement('div');
   alertEl.classList.add('alert', className);
   alertEl.appendChild(document.createTextNode(message));
