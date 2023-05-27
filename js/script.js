edd2c02a3228b88d399f72a41f168c8d9a741925
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-27 13:54:12 +0700

    Movie Details Page

--- js/script.js
+++ js/script.js
@@ -55,7 +55,6 @@ async function displayPopularMovies() {
 
     document.getElementById('popular-movies').appendChild(div);
   });
-  console.log(results);
 }
 // Display 20 popular TV Shows
 async function displayPopularTVShows() {
@@ -89,7 +88,72 @@ async function displayPopularTVShows() {
 
     document.getElementById('popular-shows').appendChild(div);
   });
-  console.log(results);
+}
+
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  console.log(movie);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+      ${
+        movie.poster_path
+          ? `<img
+        src='https://image.tmdb.org/t/p/w500${movie.poster_path}'
+        class="card-img-top"
+        alt='${movie.title}'
+      />`
+          : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt='${movie.title}'
+      />`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>${movie.overview}</p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${
+          movie.homepage
+        }" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${movie.budget.toLocaleString(
+          'en-US'
+        )}</li>
+        <li><span class="text-secondary">Revenue:</span>  $${movie.revenue.toLocaleString(
+          'en-US'
+        )}</li>
+        <li><span class="text-secondary">Runtime:</span> ${
+          movie.runtime
+        } minutes</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+        ${movie.production_companies
+          .map((company) => `<div lass="list-group">${company.name}</div>`)
+          .join('')}
+    </div>
+  `;
+  document.getElementById('movie-details').appendChild(div);
 }
 // Highlight active link:
 function highlightActiveLink() {
@@ -111,7 +175,7 @@ function init() {
       displayPopularTVShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Show Details');
