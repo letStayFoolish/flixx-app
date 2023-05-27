1d6ae45c6c286d60d020e2705d919af5e7e556e8
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-27 12:06:30 +0700

    Display Popular Movies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,52 @@
 const global = {
   currentPage: window.location.pathname,
 };
+// Fetch the data from API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'fd8d3dab48909db84857be6066ec11f1';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+  return data;
+}
+// Display popular movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('/movie/popular');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        ${
+          movie.poster_path
+            ? `<img
+          src='https://image.tmdb.org/t/p/w500${movie.poster_path}'
+          class="card-img-top"
+          alt='${movie.title}'
+        />`
+            : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt='${movie.title}'
+        />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.getElementById('popular-movies').appendChild(div);
+  });
+  console.log(results);
+}
 // Highlight active link:
 function highlightActiveLink() {
   const linkList = document.querySelectorAll('.nav-link');
@@ -15,7 +61,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('TV Shows');
